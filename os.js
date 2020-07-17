692fb2c5ed518e32a6c4c388211bbbb8ed4cd43f
sushmitakanchan <31158191+sushmitakanchan@users.noreply.github.com>
2020-07-17 17:03:43 +0530

    Day 4 - assignment 2

---
+++ os.js
@@ -0,0 +1,5 @@
+var a = prompt("Enter Os name and version seperated by space:");
+var b = a.split(" ");
+console.log(a);
+// console.log(b);
+console.log("The OS name is",''+b[0]+'',"and version is",''+b[1]);
